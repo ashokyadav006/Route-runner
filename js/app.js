dd8303a183fecdeda68c6863a55c1a3d62da24ac
ashok yadav <ashokyadav006@gmail.com>
2015-01-13 22:35:05 +0530

    Added implementation for other button

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 $(function () {
-	var map = new L.Map('map').setView([0, 0], 3);
+	var map = new L.Map('map',{
+		center: [0, 0],
+		zoom: 3,
+		minZoom: 3
+	});
 	var marker;
+	var isUserSelectingByClick = false;
+	var startPosition;
+	var endPosition;
+
 	new L.TileLayer(
 	  'http://{s}.tiles.mapbox.com/v3/osmbuildings.kbpalbpk/{z}/{x}/{y}.png',
 	  { attribution: 'Map tiles &copy; <a href="http://mapbox.com">MapBox</a>', maxNativeZoom: 19, maxZoom: 21 }
@@ -28,5 +36,24 @@ $(function () {
 
 	handleCurrentLocationError = function (err) {
 		console.log("There seems to be an error :"+err);
-	};	
+	};
+
+	function getUserSelectedPosition (event) {
+		if (!startPosition) {
+			startPosition = event.latlng;
+		} else {
+			endPosition = event.latlng;
+			map.off('click',getUserSelectedPosition);
+		}
+	}
+
+	$("#searchChoiceButton").on('click', function (event) {
+		$("#pickDestination").modal('hide');
+		if ($("#selectByClick").is(":checked")) {
+			isUserSelectingByClick = true;
+			map.on('click', getUserSelectedPosition);
+		} else {
+
+		}
+	});	
 });
