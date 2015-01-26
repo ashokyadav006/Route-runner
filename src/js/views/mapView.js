50e42d92e873cc465adb4a72bf0871e52e5dbcd8
ashok yadav <ashokyadav006@gmail.com>
2015-01-27 01:16:27 +0530

    Merge branch 'master' of https://github.com/techieashok/Route-runner

--- src/js/views/mapView.js
+++ src/js/views/mapView.js
@@ -2,11 +2,30 @@ var rr = rr || {};
 
 rr.MapView = Backbone.View.extend({
 
+	map: null,
+
 	initialize: function () {
 		this.render();
 	},
 
 	render: function () {
-		console.log("Hello");
+		map = new ol.Map({
+			target: 'map',
+			layers: [
+		      new ol.layer.Tile({
+		      	title: 'Global Imagery',
+		        source: new ol.source.OSM()
+		      })
+		    ],
+		    view: new ol.View({
+		      center: ol.proj.transform([29.027,41.029], 'EPSG:4326', 'EPSG:3857'),
+		      zoom: 11
+		    }),
+		    controls: ol.control.defaults({
+		      attributionOptions: {
+		        collapsible: false
+		      }
+		    })
+		});
 	}
 });
